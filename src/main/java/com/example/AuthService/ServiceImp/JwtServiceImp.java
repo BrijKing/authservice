459f6aed7a7a52d35b00258e90abcc1ba3585018
brijkig <brijeshvaishnav508@gmail.com>
2023-08-08 16:43:04 +0530

    handle expire token exception

--- src/main/java/com/example/AuthService/ServiceImp/JwtServiceImp.java
+++ src/main/java/com/example/AuthService/ServiceImp/JwtServiceImp.java
@@ -6,13 +6,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import com.example.AuthService.CustomExceptions.InvalidTokenException;
 import com.example.AuthService.services.JwtService;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
@@ -25,43 +24,43 @@ public class JwtServiceImp implements JwtService {
 
 	@Override
 	public ResponseEntity<String> validateToken(String token) {
-		// TODO Auto-generated method stub
+	
 		try {
+
+			Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token).toString();
+
+			return new ResponseEntity<String>("valid token", HttpStatus.OK);
+
+		} catch (ExpiredJwtException e) {
 			
-		
-		Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token).toString();
-		
-		return new ResponseEntity<String>("valid token",HttpStatus.OK);
-		
-		}catch (Exception e) {
-			// TODO: handle exception
-			return new ResponseEntity<String>("Invalid Token received!!" + e.getMessage(),HttpStatus.UNAUTHORIZED);
+			return new ResponseEntity<String>("your login session has been expired please login again !!",
+					HttpStatus.UNAUTHORIZED);
+		} catch (Exception e) {
+			
+			return new ResponseEntity<String>("Invalid Token received!!" + e.getMessage(), HttpStatus.UNAUTHORIZED);
 		}
 	}
 
 	@Override
 	public String generateToken(String email) {
-		// TODO Auto-generated method stub
+		
 		Map<String, Object> claims = new HashMap<>();
 		return createToken(claims, email);
 	}
 
 	@Override
 	public String createToken(Map<String, Object> claims, String email) {
-		// TODO Auto-generated method stub
-		return Jwts.builder()
-        .setClaims(claims)
-        .setSubject(email)
-        .setIssuedAt(new Date(System.currentTimeMillis()))
-        .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
-        .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
+		
+		return Jwts.builder().setClaims(claims).setSubject(email).setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
+				.signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
 	}
 
 	@Override
 	public Key getSignKey() {
-		// TODO Auto-generated method stub
+		
 		byte[] keyBytes = Decoders.BASE64.decode(SECRET);
-        return Keys.hmacShaKeyFor(keyBytes);
+		return Keys.hmacShaKeyFor(keyBytes);
 	}
 
 }
