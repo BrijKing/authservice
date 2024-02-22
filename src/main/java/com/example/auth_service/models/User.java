1100a28d8f6f76d38b23db0ffd7e4419fdda872f
jimit-nayee <67138265+jimit-nayee@users.noreply.github.com>
2024-02-22 15:18:13 +0530

    Update User model

--- src/main/java/com/example/auth_service/models/User.java
+++ src/main/java/com/example/auth_service/models/User.java
@@ -3,7 +3,6 @@
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
